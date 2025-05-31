d567ece2e9476b940c25d5f3fc4f8179707c5688
Gunawiartha <gugungunawiartha12@gmail.com>
2025-05-31 12:04:31 +0800

    update pertemuan 11

--- src/pertemuan10/frmTeman.java
+++ src/pertemuan10/frmTeman.java
@@ -31,6 +31,8 @@ public class frmTeman extends javax.swing.JFrame {
         TTM.setModel(TM);
         TM.addColumn("No");
         TM.addColumn("Nama Teman");
+        TM.addColumn("Alamat");
+        TM.addColumn("Telp");
         
         this.dtTemanList();
         
@@ -38,25 +40,27 @@ public class frmTeman extends javax.swing.JFrame {
         PreparedStatement PS = cnn.prepareStatement ("SELECT * FROM datateman");
         ResultSet RS = PS.executeQuery();
         
-        while (RS.next()){
-            txNama.setText(RS.getString("namateman"));
-//            txAlamat.setText(RS.getString("alamat"));
-//            txTelp.setText(RS.getString("telp"));
-        }
+//        while (RS.next()){
+//            txNama.setText(RS.getString("namateman"));
+////            txAlamat.setText(RS.getString("alamat"));
+////            txTelp.setText(RS.getString("telp"));
+//        }
     }
     
     private void dtTemanList() throws SQLException{
         Connection cnn = koneksi();
-        PreparedStatement PS = cnn.prepareStatement("SELECT idteman, namateman FROM datateman");
+        PreparedStatement PS = cnn.prepareStatement("SELECT * FROM datateman");
         ResultSet RS = PS.executeQuery();
         
         TM.getDataVector().removeAllElements();
         TM.fireTableDataChanged();
         
         while(RS.next() ){
-            Object[] dta = new Object[2];
+            Object[] dta = new Object[4];
                 dta[0] = RS.getInt("idteman");
                 dta[1] = RS.getString("namateman");
+                dta[2] = RS.getString("alamat");
+                dta[3] = RS.getString("telp");
             
             TM.addRow(dta);
         }
@@ -80,6 +84,10 @@ public class frmTeman extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         TTM = new javax.swing.JTable();
+        btnTambah = new javax.swing.JButton();
+        btnUbah = new javax.swing.JButton();
+        btnHapus = new javax.swing.JButton();
+        btnTutup = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Data Teman");
@@ -127,6 +135,24 @@ public class frmTeman extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(TTM);
 
+        btnTambah.setText("Tambah");
+
+        btnUbah.setText("Ubah");
+
+        btnHapus.setText("Hapus");
+        btnHapus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnHapusActionPerformed(evt);
+            }
+        });
+
+        btnTutup.setText("Tutup");
+        btnTutup.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTutupActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -140,15 +166,27 @@ public class frmTeman extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txTelp)
-                            .addComponent(txAlamat)
-                            .addComponent(txNama, javax.swing.GroupLayout.PREFERRED_SIZE, 291, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(19, 19, 19))))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(txTelp)
+                                .addComponent(txAlamat)
+                                .addComponent(txNama, javax.swing.GroupLayout.PREFERRED_SIZE, 291, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnTambah)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnUbah)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnHapus)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnTutup)))
+                        .addGap(18, 18, 18))))
         );
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {btnHapus, btnTambah, btnTutup, btnUbah});
+
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
@@ -164,36 +202,56 @@ public class frmTeman extends javax.swing.JFrame {
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(txAlamat, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jLabel3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txTelp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(txTelp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btnTambah, javax.swing.GroupLayout.DEFAULT_SIZE, 35, Short.MAX_VALUE)
+                            .addComponent(btnUbah, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnTutup, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnHapus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
+
+        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {btnHapus, btnTambah, btnTutup, btnUbah});
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void TTMMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TTMMouseClicked
-        String sql = "SELECT * FROM `datateman` WHERE idteman="+TTM.getValueAt(TTM.getSelectedRow(),0)+"";
-        Connection cnn = koneksi();
-        PreparedStatement PS = null;
-        try {
-            PS = cnn.prepareStatement(sql);
-        } catch (SQLException ex) {
-            Logger.getLogger(frmTeman.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try {
-            ResultSet RS = PS.executeQuery();
-            txNama.setText(RS.getString("namateman"));
-            txAlamat.setText(RS.getString("alamat"));
-            txTelp.setText(RS.getString("telp"));
-        } catch (SQLException ex) {
-            Logger.getLogger(frmTeman.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//        String sql = "SELECT * FROM `datateman` WHERE idteman="+TTM.getValueAt(TTM.getSelectedRow(),0)+"";
+//        Connection cnn = koneksi();
+//        PreparedStatement PS = null;
+//        try {
+//            PS = cnn.prepareStatement(sql);
+//        } catch (SQLException ex) {
+//            Logger.getLogger(frmTeman.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        try {
+//            ResultSet RS = PS.executeQuery();
+//            txNama.setText(RS.getString("namateman"));
+//            txAlamat.setText(RS.getString("alamat"));
+//            txTelp.setText(RS.getString("telp"));
+//        } catch (SQLException ex) {
+//            Logger.getLogger(frmTeman.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+
+          txNama.setText(TTM.getValueAt(TTM.getSelectedRow(), 1).toString() );
+          txAlamat.setText(TTM.getValueAt(TTM.getSelectedRow(), 2).toString() );
+          txTelp.setText(TTM.getValueAt(TTM.getSelectedRow(), 3).toString() );
         
     }//GEN-LAST:event_TTMMouseClicked
+
+    private void btnTutupActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTutupActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnTutupActionPerformed
+
+    private void btnHapusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHapusActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnHapusActionPerformed
 
     /**
      * @param args the command line arguments
@@ -236,6 +294,10 @@ public class frmTeman extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable TTM;
+    private javax.swing.JButton btnHapus;
+    private javax.swing.JButton btnTambah;
+    private javax.swing.JButton btnTutup;
+    private javax.swing.JButton btnUbah;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
